19cf9c13aec924d64ad74e22c0aaaae483322388
Codernood2 <zouhairchar@gmail.com>
2023-06-18 07:52:10 -0700

    4-print_alphabt

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+  * main - print all alphabe except e q
+  *Return: Always 0 (success)
+ */
+int main(void)
+{
+	char alp[26] = "abcdefghijklmnopqrstuvwxyz";
+	char e = 'e';
+	char q = 'q';
+	int i;
+
+	for (i = 0; i  < 26; i++)
+{
+	if (alp[i] != 'e' && alp[i] != 'q')
+	{
+		putchar(alp[i]);
+	}
+}
+	putchar('\n');
+	return (0);
+}
