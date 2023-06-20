4efa7e9a7e9634a4fefb601e662553b61b930595
Codernood2 <zouhairchar@gmail.com>
2023-06-19 17:05:20 -0700

    102-print_comb5.c

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -7,7 +7,7 @@ int main(void)
 {
 	int num1, num2;
 
-	for (num1 = 0; num1 < 99 ; num1++)
+	for (num1 = 0; num1 <= 98; num1++)
 	{
 		for (num2 = num1 + 1; num2 < 100; num2++)
 		{
@@ -17,11 +17,11 @@ int main(void)
 		putchar((num2 / 10) + '0');
 		putchar((num2 % 10) + '0');
 
-		if (num1 == 98 || num2 == 99)
-
+		if (num1 != 98 || num2 != 99)
+		{
 		putchar(',');
 		putchar(' ');
-
+		}
 		}
 	}
 	putchar('\n');
