497185d1dfb909e354a48843e5b86f3c5b0b1ac5
Codernood2 <zouhairchar@gmail.com>
2023-06-21 12:17:18 -0700

    2-alphabet_x10

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -2,7 +2,7 @@
 
 /**
   *print_alphabet_x10 - print alphabet 10 times
-  *@n: print 10 times
+  *
   *Return: Always 0 (success)
  */
 
@@ -10,8 +10,9 @@ void print_alphabet_x10(void)
 {
 
 	char alphabet;
+
 	int i;
-	
+
 	for (i = 0; i < 10; i++)
 	{
 		for (alphabet = 'a'; alphabet <= 'z'; alphabet++)
