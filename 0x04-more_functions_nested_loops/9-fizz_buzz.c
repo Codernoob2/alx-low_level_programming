8d4c20ab84a7c4cd9a0c7f089d28079ad2a417b2
Codernood2 <zouhairchar@gmail.com>
2023-06-23 16:36:43 -0700

    9-fizz_buzz.c

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+/**
+  *main - entry point
+  *description: print the 1 - 100
+  *fizz for multiples of 3 and buzz for multiples of 5
+  *fizzbuss for multiples of both
+  *Return: Always 0
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = 1; i <= 100; i++)
+	{
+		if (i % 15 == 0)
+			printf("fizzbuzz");
+		if (i % 3 == 0)
+			printf("fizz");
+		if (i % 5 == 0)
+			printf("buzz");
+		else
+			printf("%i", i);
+		if (i < 100)
+			printf(" ");
+	}
+	printf("\n")}
+	return (0);
+}
