302b108313d4a5b535142d7047d3b4a8c431cc52
Codernood2 <zouhairchar@gmail.com>
2023-07-09 13:02:29 -0700

    2-args.c

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+  *main - prints all arguments it recieves
+  *@argc: int
+  *@agrv: list
+  *Return: 0
+ */
+
+int main(int argc, char const *argv[])
+{
+	int i = 0;
+
+	while (agrc--)
+	{
+		printf("%s\n", argv[i]);
+		i++;
+	}
+	return (0);
+}
