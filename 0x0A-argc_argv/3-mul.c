019ead60a0b7a4ba4fc2fbdeee0b2538a7ad235c
Codernood2 <zouhairchar@gmail.com>
2023-07-09 13:57:29 -0700

    3-mul.c

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -8,19 +8,19 @@
   *Return: 0
   */
 
-int main(int argc, char *argv[])
+int main(int argc, char **argv)
 {
-	int sum;
+	int x, y;
 
-	if (argc == 3)
-	{
-	sum = atoi(argv[1]) * atoi(argv[2]);
-	printf("%d\n", sum);
-	}
-	else
+	if (argc < 3)
 	{
 		printf("error\n");
 		return (1);
 	}
+
+	x = atoi(argv[1]);
+	y = atoi(argv[2]);
+		printf("%d\n", x * y);
+
 	return (0);
 }
